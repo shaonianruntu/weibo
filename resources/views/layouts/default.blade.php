51585f90dfd913dc227af041504022576f1d408c
shaonianruntu <514574496@qq.com>
2019-08-30 20:25:26 +0800

    initialize layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,21 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel Tutorial</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   </head>
   <body>
-    @yield('content')
+    <nav class='navbar navbar-expand-lg navbar-dark bg-dark mb-5'>
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
