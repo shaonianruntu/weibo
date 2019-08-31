2ebfe74fec6948edbb6eb0a796f04612cf9ed111
shaonianruntu <514574496@qq.com>
2019-08-31 15:52:29 +0800

    增加 CSRF 验证

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,13 +5,9 @@
 @section('content')
 <div class="row">
   <div class="offset-md-2 col-md-8">
-    <div class="col-md-12">
-      <div class="offset-md-2 col-md-8">
-        <section class="user_info">
-          @include('shared._user_info', ['user' => $user])
-        </section>
-      </div>
-    </div>
+    <section class="user_info">
+      @include('shared._user_info', ['user' => $user])
+    </section>
   </div>
 </div>
 @stop
